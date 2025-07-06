d079aa423c556ed7194e9017c9103efb02118688
Jono Feist <jono@pixielabs.io>
2025-07-07 00:08:36 +0100

    Improve header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+
+// FontAwesome imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// Add all icons to the library
+library.add(fas, fab)
 
 // Create a client
 const queryClient = new QueryClient({
