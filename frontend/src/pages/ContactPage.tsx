fd09211f9609fe74b889538688c8f229d1950fe0
Jono Feist <jono@pixielabs.io>
2025-07-06 23:34:26 +0100

    Add custom contact page wip 3

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -47,34 +47,53 @@ const ContactPage: React.FC = () => {
           {contactMethods.map((contactMethod, index) => (
             <div
               key={index}
-              className="GetInTouchContactMethod row"
+              className="GetInTouchContactMethod row d-flex align-items-center"
               onClick={() => handleContactClick(contactMethod)}
               onMouseEnter={() => setHoveredIndex(index)}
               onMouseLeave={() => setHoveredIndex(null)}
             >
-              <div className="col-12 col-md-1 text-center">
-                <div className="fa-layers fa-4x" style={{ position: 'relative', display: 'inline-block' }}>
+              <div className="col-2 col-md-2 d-flex justify-content-start">
+                <div 
+                  className="fa-layers fa-4x" 
+                  style={{ 
+                    position: 'relative', 
+                    display: 'inline-block', 
+                    width: '1em', 
+                    height: '1em'
+                  }}
+                >
                   <FontAwesomeIcon 
                     icon={faSquare}
-                    style={{ color: hoveredIndex === index ? '#6c757d' : '#212529' }}
+                    style={{ 
+                      color: hoveredIndex === index ? '#6c757d' : '#212529',
+                      position: 'absolute',
+                      left: '0',
+                      top: '0',
+                      width: '100%',
+                      height: '100%'
+                    }}
                   />
                   <FontAwesomeIcon 
                     icon={contactMethod.icon}
                     style={{ 
                       position: 'absolute',
                       left: '50%',
-                      top: '50%',
-                      transform: 'translate(-50%, -50%)',
+                      top: '100%',
+                      transform: contactMethod.transform?.includes('shrink') 
+                        ? 'translate(-50%, -50%) scale(0.5)' 
+                        : 'translate(-50%, -50%) scale(0.7)',
                       color: 'white',
-                      fontSize: contactMethod.transform?.includes('shrink') ? '0.75em' : '1em'
+                      zIndex: 1
                     }}
                   />
                 </div>
               </div>
-              <div className="col-12 col-md-11 d-flex align-items-center justify-content-center mt-3 mt-md-0">
-                <h5 style={{ color: hoveredIndex === index ? '#6c757d' : 'inherit' }}>
-                  {contactMethod.name}
-                </h5>
+              <div className="col-10 col-md-10 d-flex align-items-center">
+                <div className="w-100 text-center">
+                  <h5 style={{ color: hoveredIndex === index ? '#6c757d' : 'inherit', margin: 0 }}>
+                    {contactMethod.name}
+                  </h5>
+                </div>
               </div>
             </div>
           ))}
