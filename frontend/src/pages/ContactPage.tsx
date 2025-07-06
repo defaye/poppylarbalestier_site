c64c48ebf889294463331f7cf6caa6a22f953106
Jono Feist <jono@pixielabs.io>
2025-07-06 23:47:02 +0100

    Fix contact methods

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -39,65 +39,52 @@ const ContactPage: React.FC = () => {
   }
 
   return (
-    <div className="container">
-      <h1>Get In Touch</h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-center mb-8">Get In Touch</h1>
       
-      <div className="row d-flex">
-        <div className="col-12 col-md-8 col-lg-6 mx-auto">
-          {contactMethods.map((contactMethod, index) => (
-            <div
-              key={index}
-              className="GetInTouchContactMethod row d-flex align-items-center"
-              onClick={() => handleContactClick(contactMethod)}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
-              <div className="col-2 col-md-2 d-flex justify-content-start">
-                <div 
-                  className="fa-layers fa-4x" 
+      <div className="max-w-2xl mx-auto">
+        {contactMethods.map((contactMethod, index) => (
+          <div
+            key={index}
+            className="GetInTouchContactMethod flex items-center justify-center gap-6 py-4 cursor-pointer"
+            onClick={() => handleContactClick(contactMethod)}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <div className="flex-shrink-0">
+              <div 
+                className="fa-layers fa-4x relative inline-block w-16 h-16"
+              >
+                <FontAwesomeIcon 
+                  icon={faSquare}
+                  className="absolute inset-0 w-full h-full"
                   style={{ 
-                    position: 'relative', 
-                    display: 'inline-block', 
-                    width: '1em', 
-                    height: '1em'
+                    color: hoveredIndex === index ? '#6c757d' : '#212529'
                   }}
-                >
-                  <FontAwesomeIcon 
-                    icon={faSquare}
-                    style={{ 
-                      color: hoveredIndex === index ? '#6c757d' : '#212529',
-                      position: 'absolute',
-                      left: '0',
-                      top: '0',
-                      width: '100%',
-                      height: '100%'
-                    }}
-                  />
-                  <FontAwesomeIcon 
-                    icon={contactMethod.icon}
-                    style={{ 
-                      position: 'absolute',
-                      left: '50%',
-                      top: '100%',
-                      transform: contactMethod.transform?.includes('shrink') 
-                        ? 'translate(-50%, -50%) scale(0.5)' 
-                        : 'translate(-50%, -50%) scale(0.7)',
-                      color: 'white',
-                      zIndex: 1
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="col-10 col-md-10 d-flex align-items-center">
-                <div className="w-100 text-center">
-                  <h5 style={{ color: hoveredIndex === index ? '#6c757d' : 'inherit', margin: 0 }}>
-                    {contactMethod.name}
-                  </h5>
-                </div>
+                />
+                <FontAwesomeIcon 
+                  icon={contactMethod.icon}
+                  className="absolute inset-0 flex items-center justify-center"
+                  style={{ 
+                    left: '50%',
+                    top: '110%',
+                    transform: 'translate(-50%, -50%) scale(0.5)',
+                    color: 'white',
+                    zIndex: 1
+                  }}
+                />
               </div>
             </div>
-          ))}
-        </div>
+            <div className="flex-grow text-center">
+              <h5 
+                className="text-lg font-serif m-0"
+                style={{ color: hoveredIndex === index ? '#6c757d' : 'inherit' }}
+              >
+                {contactMethod.name}
+              </h5>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
