c1ff55bae69c080e798ac174f3950d74874917a3
Jono Feist <jono@pixielabs.io>
2025-07-06 21:30:33 +0100

    Frontend v2 wip4

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ApiResponse, NavigationItem, Page, Post, Category, Tag } from '@/types';
+import { NavigationItem, Page, Post, Category, Tag } from '@/types';
 
 const api = axios.create({
   baseURL: '/api',
